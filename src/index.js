850fb2c4b877e432783e82d9afb41de726e4bc55
Ivan Gusak <ivan.gusak10@gmail.com>
2021-08-11 11:54:48 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 
 exports.min = function min (array) {
-  if (array == '' || array === [] || array[0] == undefined || array.length == 0 ){
-     return 0;
-  }  
-  let answer = array.sort( (a, b) => a - b );
- return answer[0];
-  }
- 
+  if (array === undefined || !array.length) return 0;
+  return Math.min.apply(null, array);
+}
 
+exports.avg = function max (array) {
+  if (array === undefined || array.length < 1) return 0;
+  return array.reduce((a, b) => (a + b)) / array.length;
+}
 
-exports.max = function max (array) {
-  if (array === '' || array === ' ')  {
-    return 0;
- }
- let answer = array.sort( (a, b) => a - b );
- return answer[array.length - 1];
-  }
-
-
-
-exports.avg = function avg (array) {
-  if (array === '' || array === ' ')  {
-    return 0;
- }
- let sum = 0;
- for ( let i = 0; i < array.length; i++ ) {
-   sum+= array[i];
- }
- return sum / array.length;
+exports.max = function avg (array) {
+  if (array === undefined || !array.length) return 0;
+  return Math.max.apply(null, array);
 }
